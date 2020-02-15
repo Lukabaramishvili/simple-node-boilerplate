d4148fe9b71bcec30032e9a95a0d11043a02dbb9
github username <github email address>
2020-02-14 20:33:25 -0500

    added html

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,13 @@
 // jest won't error out on DOM calls.
-console.log(document.querySelector('p').innerHTML);
+// console.log(document.querySelector('p').innerHTML);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  getReservations()
+
+});
+  function getReservations() {
+    fetch("http://localhost:3000/reservations")
+    .then(res => res.json())
+    .then(reservations => {console.log(reservations)})
+  }
--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -6,4 +6,15 @@ router.get('/', async (req,res) => {
   res.json(await Reservation.all());
 });
 
+router.post('/book', async (req,res) => {
+  const newRes = await Reservation.create({
+    name: DataTypes.STRING,
+    slot: DataTypes.DATE
+  })
+});
+
+
+
+
+
 module.exports = router;
